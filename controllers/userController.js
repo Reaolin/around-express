1d416227847ff2312c0b0c0259e59acc94f2fc13
Royal Paschall <royal.paschall@gmail.com>
2021-02-17 10:18:34 -0500

    fixing error

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,87 @@
-
-const User = require('../models/users')
+const User = require("../models/user");
 
 function getUser(req, res) {
   return User.find({})
-  .then((users) => {
-    res.status(200).send(users);
-  })
-  .catch((err) => res.status(400).send(err));
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => res.status(400).send(err));
 }
 
 function getOneUser(req, res) {
-  return User.find({id: req.params.id})
-  .then((user) => {
-    if (user) {
-      return res.status(200).send(user);
-    }
-    return res.status(404).send({ message: 'This is not the user you are looking for' });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-       res.status(400).send({ message: 'Bad Request'});
-    }else {
-      res.status(500).send({ message: 'Internal Server Error'})
-    }
+  return User.find({ id: req.params.id })
+    .then((user) => {
+      if (user) {
+        return res.status(200).send(user);
+      }
+      return res
+        .status(404)
+        .send({ message: "This is not the user you are looking for" });
     })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(400).send({ message: err });
+      } else {
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
 }
 
 const createUser = (req, res) => {
-  const {name, about, avatar } = req.body;
+  const { name, about, avatar } = req.body;
+  console.log(req.body);
   User.create({ name, about, avatar })
-  .then((user) => res.status(200).send(user)
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: err });
+      }
+      res.status(500).send({ message: err });
+    });
+};
+
+/*const createUser = (req, res) => {
+  console.log(req.body, 'this is the body');
+  return User.countDocuments({})
+  .then( id => {
+    return User.create({...req.body, id})
+  })
+  .then(user => {
+    res.status(200).send(user)
+  })
   .catch((err) => {
     if (err.name === 'ValidationError'){
       res.status(400).send({ message: err });
     }
     res.status(500).send({ message: err });
-  }))
-}
-
-const updateUser = (req, res) => {
-  User.findByIdAndUpdate(req.params.id, { name: req.params.name, about: req.params.about})
-  .then((user) => {
-    if (!user) {
-      res.status(400).send({ message: 'This is not the user you are looking for'})
-    }
-    res.status(200).send({data: user})})
-  .catch((err) => {
-    if (err.name === 'ValidationError'){
-      res.status(400).send(err);
-    }
-    res.status(500).send(err);
   })
 }
+*/
+const updateUser = (req, res) => {
+  User.findByIdAndUpdate(req.params.id, {
+    name: req.params.name,
+    about: req.params.about,
+    avatar: req.params.avatar,
+  })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(400)
+          .send({ message: "This is not the user you are looking for" });
+      }
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send(err);
+      }
+      res.status(500).send(err);
+    });
+};
 
 module.exports = {
   getUser,
   getOneUser,
   createUser,
-  updateUser
+  updateUser,
 };
--- routers/users.js
+++ routers/users.js
@@ -4,7 +4,6 @@ const userRouter = express.Router();
 const { getUser, getOneUser, createUser, updateUser } = require('../controllers/userController');
 
 userRouter.get('/', getUser);
-
 userRouter.get('/:_id', getOneUser);
 userRouter.post('/', createUser);
 userRouter.patch('/me', updateUser);
